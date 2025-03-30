28d8d24f95f61e6720cfe9e835dfb37dd238c0c6
Phamminhtrikp <tripham.210504@gmail.com>
2025-03-30 17:34:54 +0700

    fix bug obj to array

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -12,7 +12,7 @@ const taskSkema = new mongoose.Schema({
     priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' },
     status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
     dueDate: { type: Date, required: true },
-    assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to the user assigned to the task
+    assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Reference to the user assigned to the task
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to the user who created the task
     attachments: [{ type: String }], // Array of attachment URLs
     todoCheckList: [todoSchema], // Array of todo items
