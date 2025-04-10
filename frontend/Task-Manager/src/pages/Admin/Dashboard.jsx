c61185e401cc4ac4673451e60b98de72a7bd9758
Phamminhtrikp <tripham.210504@gmail.com>
2025-04-10 19:59:11 +0700

    API call to fetch dashboard data

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router';
 import { useUserAuth } from '../../hooks/useUserAuth';
 import { UserContext } from '../../context/userContext';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 
 const Dashboard = () => {
@@ -16,7 +18,23 @@ const Dashboard = () => {
   const [pieChartData, setPieChartData] = useState([]);
   const [barChartData, setBarChartData] = useState([]);
 
-  
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_DASHBOARD_DATA);
+      if(response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardData();
+
+    return () => {};
+  }, []);
+
 
   return (
     <DashboardLayout activeMenu="Dashboard">
