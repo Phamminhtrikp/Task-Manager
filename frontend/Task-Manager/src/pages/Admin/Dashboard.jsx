d283c97f70446b6f3ccc0c389de7d525b9893477
Phamminhtrikp <tripham.210504@gmail.com>
2025-04-10 19:55:47 +0700

    create dashboard page layout

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -1,15 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import { useUserAuth } from '../../hooks/useUserAuth';
 import { UserContext } from '../../context/userContext';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+
 
 const Dashboard = () => {
   useUserAuth();
 
   const { user } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+
+  
+
   return (
-    <div>
-      Dashboard
-    </div>
+    <DashboardLayout activeMenu="Dashboard">
+      
+    </DashboardLayout>
   )
 }
 
