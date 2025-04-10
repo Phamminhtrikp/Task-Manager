f5aa57097b9bc311548e5f3b53bb8b31894bf2dd
Phamminhtrikp <tripham.210504@gmail.com>
2025-04-10 21:10:41 +0700

    Create Recent Tasks Section

--- frontend/Task-Manager/src/utils/helper.js
+++ frontend/Task-Manager/src/utils/helper.js
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
+};
+
+
+export const addThounsandsSeparator = (num) => {
+    if(num === null || isNaN(num)) return "";
+
+    const [integerPart, fractionalPart] = num.toString().split(".");
+    const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return fractionalPart
+        ? `${formattedInteger}.${fractionalPart}`
+        : formattedInteger;
 };
