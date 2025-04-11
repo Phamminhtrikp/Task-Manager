dd3df2e2ebb93f01645780aa5753aa8a01d64668
Phamminhtrikp <tripham.210504@gmail.com>
2025-04-11 23:55:28 +0700

    API Call to Get All Tasks

--- frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
+++ frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { LuCircleArrowDown, LuFileSpreadsheet } from 'react-icons/lu';
+import TaskStatusTabs from '../../components/TaskStatusTabs';
+import TaskCard from '../../components/Cards/TaskCard';
 
 const ManageTasks = () => {
+  const [allTasks, setAllTasks] = useState([]);
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState("All");
+
+  const navigate = useNavigate();
+
+  const getAllTasks = async () => {
+
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: {
+          status: filterStatus === "All" ? "" : filterStatus,
+        },
+      });
+
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
+
+      // Map statusSummary data with fixed labels and order
+      const statusSummary = response.data?.statusSummary || {};
+
+      const statusArray = [
+        { label: "All", count: statusSummary.all || 0 },
+        { label: "Pending", count: statusSummary.pending || 0 },
+        { label: "In Progress", count: statusSummary.inProgress || 0 },
+        { label: "Completed", count: statusSummary.completed || 0 },
+      ];
+
+      setTabs(statusArray);
+
+    } catch (error) {
+      console.error("Error to fetching data!", error);
+    }
+  };
+
+  const handleClick = (taskData) => {
+    navigate(`/admin/create-task`, { state: { taskId: taskData._id } });
+  };
+
+  // Download task report
+  const handleDownloadReport = async () => { };
+
+  useEffect(() => {
+    getAllTasks(filterStatus);
+    return () => { };
+  }, [filterStatus]);
+
   return (
-    <div>
-      Manage Tasks
-    </div>
+    <DashboardLayout activeMenu={"Manage Tasks"}>
+      <div className="my-5">
+        <div className="flex flex-col lg:flex-row lg:items-center justify-between">
+          <div className="flex items-center justify-between gap-3">
+            <h2 className="text-xl md:text-xl font-medium">My Tasks</h2>
+
+            <button
+              className="flex lg:hidden group download-btn"
+              onClick={handleDownloadReport}
+            >
+              <LuCircleArrowDown className='text-lg group-hover:animate-bounce' />
+              Download Report
+              <LuFileSpreadsheet className="text-lg group-hover:animate-pulse" />
+            </button>
+          </div>
+
+          
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
