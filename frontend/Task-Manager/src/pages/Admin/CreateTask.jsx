8abccc80b997b9cab5edbaa9947bf99bfd16046d
Phamminhtrikp <tripham.210504@gmail.com>
2025-04-11 17:03:03 +0700

    Build create task form

--- frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
+++ frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
@@ -1,10 +1,205 @@
-import React from 'react'
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useLocation, useNavigate } from 'react-router';
+import moment from 'moment';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import { LuTrash2 } from 'react-icons/lu';
+import { PRIORITY_DATA } from '../../utils/data';
+import SelectDropdown from '../../components/Inputs/SelectDropdown';
+import SelectUsers from '../../components/Inputs/SelectUsers';
+import TodoListInput from '../../components/Inputs/TodoListInput';
+import AddAttachmentsInput from '../../components/Inputs/AddAttachmentsInput';
 
 const CreateTask = () => {
+
+  const location = useLocation();
+  const { taskId } = location.state || {};
+  const navigate = useNavigate();
+
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: "",
+    priority: "Low",
+    dueDate: null,
+    assignedTo: [],
+    todoCheckList: [],
+    attachments: [],
+  });
+
+  const [currentTask, setCurrentTask] = useState(null);
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
+
+  const handleValueChange = (key, value) => {
+    setTaskData((prevData) => ({
+      ...prevData, [key]: value
+    }));
+  };
+
+  const clearData = () => {
+    // REset form
+    setTaskData({
+      title: "",
+      description: "",
+      priority: "Low",
+      dueDate: null,
+      assignedTo: [],
+      todoCheckList: [],
+      attachments: [],
+    });
+  };
+
+  // Create task
+  const createTask = async () => { };
+
+  // Update Task
+  const updateTask = async () => { };
+
+  // get Task info by id
+  const getTaskDetailsByID = async () => { };
+
+  // Delete task
+  const deleteTask = async () => {
+
+  };
+
+  // Handle Submit
+  const handleSubmit = async () => {
+    
+  }
+
+  console.log("ERR >> ", error)
+
   return (
-    <div>
-      Create Task
-    </div>
+    <DashboardLayout activeMenu={"Create Task"}>
+      <div className="mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+          <div className="form-card col-span-3">
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl md:text-xl font-medium">
+                {taskId ? "Update Task" : "Create Task"}
+              </h2>
+
+              {taskId && (
+                <button
+                  className="flex items-center gap-1.5 textx-[13px] font-medium text-rose-500 bg-rose-50 border border-rose-100 hover:border-rose-300 cursor-pointer"
+                  onClick={() => setOpenDeleteAlert(true)}
+                >
+                  <LuTrash2 className="text-base" /> Delete
+                </button>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <label className="text-xs font-medium text-slate-600">
+                Task Title
+              </label>
+
+              <input
+                placeholder='Create App UI'
+                className='form-input'
+                value={taskData.title}
+                onChange={({ target }) =>
+                  handleValueChange("title", target.value)}
+              />
+            </div>
+
+            <div className="mt-3">
+              <label className={"text-xs font-medium text-slate-600"}>Description</label>
+
+              <textarea
+                name=""
+                id=""
+                className="form-input"
+                placeholder='Description Task'
+                rows={4}
+                value={taskData.description}
+                onChange={({ target }) =>
+                  handleValueChange("description", target.value)
+                }
+              ></textarea>
+            </div>
+
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-span-6 md:col-span-4">
+                <label className="text-xs font-medium text-slate-600">Priority</label>
+
+                <SelectDropdown
+                  options={PRIORITY_DATA}
+                  value={taskData.priority}
+                  onChange={(value) => handleValueChange("priority", value)}
+                  placeholder={"Select Priority"}
+                />
+              </div>
+
+              <div className="col-span-6 md:col-span-4">
+                <label className="text-xs font-medium text-slate-600">
+                  Due Date
+                </label>
+
+                <input
+                  className='form-input'
+                  placeholder='Create App UI'
+                  value={taskData?.dueDate ?? ""}
+                  onChange={({ target }) =>
+                    handleValueChange("dueDate", target.value)}
+                  type='date'
+                />
+              </div>
+
+              <div className="col-span-12 md:col-span-3">
+                <label className="text-xs font-medium text-slate-600">
+                  Assign to
+                </label>
+
+                <SelectUsers
+                  selectedUsers={taskData.assignedTo}
+                  setSelectedUsers={(value) => {
+                    handleValueChange("assignedTo", value);
+                  }}
+                />
+              </div>
+            </div>
+
+            {/*  */}
+            <div className="mt-3">
+              <label className="text-xs font-medium text-slate-600">TODO CheckList</label>
+
+              <TodoListInput
+                todoList={taskData?.todoCheckList}
+                setTodoList={(value) => handleValueChange("todoCheckList", value)}
+              />
+            </div>
+
+            {/* _ */}
+            <div className="mt-3">
+              <label className="text-xs text-slate-600 font-medium">Add Attachments</label>
+
+              <AddAttachmentsInput
+                attachments={taskData?.attachments}
+                setAttachments={(value) => { handleValueChange("attachments", value) }}
+              />
+            </div>
+
+            {/* Error */}
+            {error && (<p className="text-red-500 font-medium text-xs mt-5">{error}</p>)}
+
+            <div className="flex justify-end mt-7">
+              <button
+                className="add-btn"
+                onClick={handleSubmit}
+                disabled={loading}
+              >
+                {taskId ? "UPDATE TASK" : "CREATE TASK"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
