9563efc68db20bc239906c1cefaea637658c8f73
Phamminhtrikp <tripham.210504@gmail.com>
2025-04-13 13:50:59 +0700

    Calling get all users details api

--- frontend/Task-Manager/src/pages/Admin/ManageUsers.jsx
+++ frontend/Task-Manager/src/pages/Admin/ManageUsers.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 const ManageUsers = () => {
+  const [allUsers, setAllUsers] = useState([]);
+
+  const getAllUsers = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.USERS.GET_ALL_USERS);
+      if (response.data?.length > 0) {
+        setAllUsers(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  // Download report
+  const handleDownloadReport = () => { };
+
+  useEffect(() => {
+    getAllUsers();
+
+    return () => { };
+  }, [])
+
   return (
     <div>
       ManageUser
